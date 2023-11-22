fdb9f5bea83ee761fc20957a748ef4f6896647f2
jay9044 <jamalwestfield@gmail.com>
2023-11-22 14:01:00 +0000

    file clean up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,8 +22,8 @@ export function Footer() {
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About Me</NavLink>
-                <NavLink href="/about">Articles</NavLink>
-                <NavLink href="/about">Certificates</NavLink>
+                <NavLink href="/articles">Articles</NavLink>
+                <NavLink href="/certs">Certificates</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Jamal Westfield. All rights
