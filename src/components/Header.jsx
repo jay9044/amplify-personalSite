6d97b27f955ac86063ba70a5425825f7537c7875
jay9044 <jamalwestfield@gmail.com>
2023-11-22 18:25:52 +0000

    udpate header verbiage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -124,7 +124,7 @@ function MobileNavigation(props) {
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                <MobileNavItem href="/about">About Me</MobileNavItem>
+                <MobileNavItem href="/about">About</MobileNavItem>
                 <MobileNavItem href="/articles">Articles</MobileNavItem>
                 <MobileNavItem href="/certs">Certificates</MobileNavItem>
                 <MobileNavItem href="/projects">Projects</MobileNavItem>
@@ -164,7 +164,7 @@ function DesktopNavigation(props) {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/about">About Me</NavItem>
+        <NavItem href="/about">About</NavItem>
         <NavItem href="/articles">Articles</NavItem>
         <NavItem href="/certs">Certifiactes</NavItem>
         <NavItem href="/projects">Projects</NavItem>
