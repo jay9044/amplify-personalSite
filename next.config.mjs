7cf62f6655eeecdf6121b34fe9974ddf79ac44e0
jay9044 <jamalwestfield@gmail.com>
2024-01-04 19:37:18 +0000

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: 'out',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,4 @@
-import Image from 'next/image'
 import Link from 'next/link'
-import clsx from 'clsx'
-
 import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
